6e6fcb811bd2e4fe8db7cd3263bb22476905bd8d
Simon Godard <sgodard@cloudops.com>
2015-11-27 16:00:07 -0500

    MC-3658: Added provider skeleton and instance resource

--- provider.go
+++ provider.go
@@ -1,4 +1,4 @@
-package main
+package cloudca
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
@@ -6,19 +6,32 @@ import (
 )
 
 func Provider() terraform.ResourceProvider {
-	return &schema.Provider {
+	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
 			"api_url": &schema.Schema{
-				Type: schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("CLOUDCA_API_URL", nil),
 			},
 			"api_key": &schema.Schema{
-				Type: schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("CLOUDCA_API_KEY", nil),
 			},
 		},
-		ResourcesMap: map[string]*schema.Resource{},
+		ResourcesMap: map[string]*schema.Resource{
+			"cloudca_instance": resourceCloudcaInstance(),
+		},
+
+		ConfigureFunc: providerConfigure,
 	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := Config{
+		APIURL: d.Get("api_url").(string),
+		APIKey: d.Get("api_key").(string),
+	}
+
+	return config.NewClient()
 }
