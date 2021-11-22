f645b8c2e4aaf794f083f3288f9e3179400c4255
lamminade <leslie.dawson@mail.mcgill.ca>
2021-11-22 13:36:36 -0500

    Add rest of tests from branch testacc with updated imports

---
+++ cloudca/resource_cloudca_load_balancer_rule_test.go
@@ -0,0 +1,126 @@
+package cloudca
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/cloud-ca/go-cloudca"
+)
+
+func TestAccLoadBalancerRuleCreate(t *testing.T) {
+	t.Parallel()
+
+	environmentID := "a225a598-f440-439e-a51e-1c5275bc6d57"
+	vpcID := "438fe7a0-d7a6-44f8-875d-b976021a6ae4"
+	networkID := "1d5c1e64-59f1-4a34-8539-77af5153058c"
+	instanceName := fmt.Sprintf("terraform-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLoadBalancerRuleCreateDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLoadBalancerRuleCreate(environmentID, vpcID, networkID, instanceName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLoadBalancerRuleCreateExists("cloudca_load_balancer_rule.foobar"),
+				),
+			},
+			{
+				ResourceName:      "cloudca_load_balancer_rule.foobar",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccLoadBalancerRuleCreate(environment, vpc, network, name string) string {
+	return fmt.Sprintf(`
+resource "cloudca_instance" "foobar" {
+	environment_id   = "%s"
+	network_id       = "%s"
+	name             = "%s"
+	template         = "Ubuntu 18.04.2"
+	compute_offering = "Standard"
+	cpu_count        = 1
+	memory_in_mb     = 1024
+}
+resource "cloudca_public_ip" "foobar" {
+	environment_id = "%s"
+	vpc_id         = "%s"
+}
+resource "cloudca_load_balancer_rule" "foobar" {
+	environment_id = "%s"
+    network_id     = "%s"
+    name           = "%s"
+	public_ip_id   = "${cloudca_public_ip.foobar.id}"
+    protocol       = "tcp"
+    algorithm      = "leastconn"
+    public_port    = 80
+    private_port   = 80
+    instance_ids   = ["${cloudca_instance.foobar.id}"]
+}`, environment, network, name, environment, vpc, environment, network, name)
+}
+
+func testAccCheckLoadBalancerRuleCreateExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		if rs.Primary.Attributes["environment_id"] == "" {
+			return fmt.Errorf("Environment ID is missing")
+		}
+
+		client := testAccProvider.Meta().(*cca.CcaClient)
+		resources, err := getResourcesForEnvironmentID(client, rs.Primary.Attributes["environment_id"])
+		if err != nil {
+			return err
+		}
+
+		found, err := resources.LoadBalancerRules.Get(rs.Primary.ID)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		if found.Id != rs.Primary.ID {
+			return fmt.Errorf("Load Balancer Rule not found")
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckLoadBalancerRuleCreateDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*cca.CcaClient)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type == "cloudca_load_balancer_rule" {
+			if rs.Primary.Attributes["environment_id"] == "" {
+				return fmt.Errorf("Environment ID is missing")
+			}
+
+			resources, err := getResourcesForEnvironmentID(client, rs.Primary.Attributes["environment_id"])
+			if err != nil {
+				return err
+			}
+
+			_, err = resources.LoadBalancerRules.Get(rs.Primary.ID)
+			if err == nil {
+				return fmt.Errorf("Load Balancer Rule still exists")
+			}
+		}
+	}
+
+	return nil
+}
---
+++ cloudca/resource_cloudca_network_acl_rule_test.go
@@ -0,0 +1,118 @@
+package cloudca
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/cloud-ca/go-cloudca"
+)
+
+func TestAccNetworkACLRuleCreate(t *testing.T) {
+	t.Parallel()
+
+	environmentID := "a225a598-f440-439e-a51e-1c5275bc6d57"
+	vpcID := "438fe7a0-d7a6-44f8-875d-b976021a6ae4"
+	networkACLRuleName := fmt.Sprintf("terraform-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNetworkACLRuleCreateDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkACLRuleCreate(environmentID, vpcID, networkACLRuleName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkACLRuleCreateExists("cloudca_network_acl_rule.foobar"),
+				),
+			},
+			{
+				ResourceName:      "cloudca_network_acl_rule.foobar",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccNetworkACLRuleCreate(environment, vpc, name string) string {
+	return fmt.Sprintf(`
+resource "cloudca_network_acl" "foobar" {
+	environment_id = "%s"
+	vpc_id         = "%s"
+	name           = "%s"
+	description    = "This is a %s acl"
+}
+resource "cloudca_network_acl_rule" "foobar" {
+	environment_id = "%s"
+	network_acl_id = "${cloudca_network_acl.foobar.id}"
+	rule_number    = 55
+	cidr           = "10.212.208.0/22"
+	action         = "Allow"
+	protocol       = "TCP"
+	start_port     = 80
+	end_port       = 80
+	traffic_type   = "Ingress"
+}`, environment, vpc, name, name, environment)
+}
+
+func testAccCheckNetworkACLRuleCreateExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		if rs.Primary.Attributes["environment_id"] == "" {
+			return fmt.Errorf("Environment ID is missing")
+		}
+
+		client := testAccProvider.Meta().(*cca.CcaClient)
+		resources, err := getResourcesForEnvironmentID(client, rs.Primary.Attributes["environment_id"])
+		if err != nil {
+			return err
+		}
+
+		found, err := resources.NetworkAclRules.Get(rs.Primary.ID)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		if found.Id != rs.Primary.ID {
+			return fmt.Errorf("Network ACL Rule not found")
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckNetworkACLRuleCreateDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*cca.CcaClient)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type == "cloudca_network_acl_rule" {
+			if rs.Primary.Attributes["environment_id"] == "" {
+				return fmt.Errorf("Environment ID is missing")
+			}
+
+			resources, err := getResourcesForEnvironmentID(client, rs.Primary.Attributes["environment_id"])
+			if err != nil {
+				return err
+			}
+
+			_, err = resources.NetworkAclRules.Get(rs.Primary.ID)
+			if err == nil {
+				return fmt.Errorf("Network ACL Rule still exists")
+			}
+		}
+	}
+
+	return nil
+}
